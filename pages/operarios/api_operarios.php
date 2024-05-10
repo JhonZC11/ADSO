a4f2359aeef53478fee006610e6227811167c0b6
Jhon <jhonalzate201726@gmail.com>
2024-05-10 16:50:02 -0500

    Se crea api para operarios

---
+++ pages/operarios/api_operarios.php
@@ -0,0 +1,36 @@
+<?php
+require "../php/Database.class.php";
+require "o_operarios.php";
+
+
+if($_SERVER['REQUEST_METHOD'] == 'POST' 
+&& isset($_POST['cc']) && isset($_POST['nombres'])
+&& isset($_POST['apellidos'])  && isset($_POST['tele'])
+&& isset($_POST['dir']) && isset($_POST['correo'])){
+    $operario->insert($_POST['cc'],$_POST['nombres'], $_POST['apellidos'], $_POST['tele'], $_POST['dir'], $_POST['correo']);
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'DELETE' && isset($_GET['id']) ){
+    $operario->delete($_GET['id']);
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'PUT' 
+&& isset($_GET['id']) && isset($_GET['nombres']) && isset($_GET['apellidos'])  && isset($_GET['tele']) && isset($_GET['dir']) && isset($_GET['correo'])){
+    $operario->update($_GET['id'],$_GET['nombres'], $_GET['apellidos'], $_GET['tele'], $_GET['dir'], $_GET['correo']);
+}
+
+if($_SERVER['REQUEST_METHOD']== 'GET'){
+    function get_all_clients(){
+        $database = new Database();
+        $conn = $database->getConnection();
+        $stmt = $conn->prepare('SELECT * FROM operarios');
+        if($stmt->execute()){
+            $result = $stmt->fetchAll();
+            header('HTTP/1.1 201 OK');
+            echo json_encode($result);
+        } else {
+            header('HTTP/1.1 404 No se ha podido consultar los clientes');
+        }
+    }
+    get_all_clients();
+}
--- pages/operarios/process_update.php
+++ pages/operarios/process_update.php
@@ -7,4 +7,5 @@ $apellidos=$_POST["ape"];
 $dir=$_POST["direccion"];
 $tel=$_POST["telefono"];
 $correo=$_POST["mail"];
-$operario->update($conn, $id, $nombres, $apellidos, $tel, $dir, $correo);
+$operario->update($id, $nombres, $apellidos, $tel, $dir, $correo);
+
